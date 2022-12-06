7deba8c231f08a8b2cc855ac75f739dfca2fffff
frsv14 <naf.freddy04@gmail.com>
2022-12-06 11:19:43 +0100

    deppressing
    added a text box and after that focused on the looks (isn't much yet)

---
+++ indexjs.js
@@ -0,0 +1,13 @@
+let input_text = document.getElementsByName("input_text");
+
+window.onload = function(){
+
+    let Buttonresult = document.querySelector('body');
+    document.addEventListener('keydown', key)
+
+    function key(e){
+        alert(e.key);
+    }
+
+}
+input_text  
